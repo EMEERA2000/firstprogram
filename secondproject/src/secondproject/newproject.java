334d3424e9d33b3da5f77e56bd7d3cf821a0c8f2
SRIRAM <SRIRAM@DESKTOP-UNK8EIU>
2021-09-02 15:40:52 +0530

    second

---
+++ secondproject/src/secondproject/newproject.java
@@ -0,0 +1,10 @@
+package secondproject;
+
+public class newproject {
+        
+        public static void main(String[] args)
+        {
+        	System.out.println("hello");
+        }
+        
+}
